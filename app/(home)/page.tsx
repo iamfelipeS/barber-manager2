890cb9860902e0de8ba2e7dd12a4ebaaf8ead236
iamfelipes <flipe.silva@terra.com.br>
2024-01-31 20:59:11 -0300

    welcome message add

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
 import Header from "../_components/header";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 export default function Home() {
   return (
     <div>
       <Header />
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Felipe</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EE, d 'de' MMMM 'de' yyyy",
+            { locale: ptBR })
+          }
+        </p>
+      </div>
     </div>
   );
 }
