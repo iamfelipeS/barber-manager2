f3d28f1350de6e3c46647e9bc939942018cee637
iamfelipes <flipe.silva@terra.com.br>
2024-02-05 11:04:54 -0300

    barbershop-item add

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,7 +2,14 @@ import Header from "../_components/header";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
-export default function Home() {
+import BookingItem from "../_components/ui/booking-item";
+import BarbershopItem from "./_components/barbershop-item";
+import { db } from "../lib/prisma";
+
+
+export default async function Home() {
+  const barbershops = await db.barbershop.findMany({})
+
   return (
     <div>
       <Header />
@@ -17,6 +24,20 @@ export default function Home() {
 
       <div className="px-5 mt-6">
           <Search />
+      </div>
+
+      <div className="px-5 mt-6">
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+        <BookingItem />
+      </div>
+
+      <div className="mt-6">
+          <h2 className="px-5text-xs px-5 mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
+          <div className="flex gap-4 px-5 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+            {barbershops.map((barbershop) => (
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            ))}
+          </div>
       </div>
     </div>
   );
