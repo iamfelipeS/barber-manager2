f4a7c4bd412712b2512725fda89318802e28c41f
iamfelipes <flipe.silva@terra.com.br>
2024-02-05 12:46:45 -0300

    fix bg footer

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (  
-        <div className="flex w-full bg-secondary py-6 px-5 items-center justify-center">
-            <p className="text-center text-xs opacity-75 text-gray-400">Desenvolvido por <a href="http://4codesolutions.com" target="_blank" rel="noopener noreferrer"> 4 Code Solutions </a></p>
-            <p className="text-center text-xs opacity-75 text-gray-400">© 2024 Barber Manager - Todos os direitos reservados.</p>
+        <div className="w-full bg-secondary py-6 px-5">
+            <p className="text-center text-xs opacity-75 mb-1 text-gray-400">Desenvolvido por <a className="underline" href="http://4codesolutions.com" target="_blank" rel="noopener noreferrer"> 4 Code Solutions</a></p>
+            <p className="text-center text-xs opacity-75 text-gray-400">© 2024 Barber Manager V2 - Todos os direitos reservados.</p>
         </div>
     );
 }
