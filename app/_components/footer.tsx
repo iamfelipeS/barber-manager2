2d2d1ea3cb3b476c7b3ada35f66d39c4410e13b8
iamfelipes <flipe.silva@terra.com.br>
2024-02-05 11:48:21 -0300

    add footer

---
+++ app/_components/footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (  
+        <div className="flex w-full bg-secondary py-6 px-5 items-center justify-center">
+            <p className="text-center text-xs opacity-75 text-gray-400">Desenvolvido por <a href="http://4codesolutions.com" target="_blank" rel="noopener noreferrer"> 4 Code Solutions </a></p>
+            <p className="text-center text-xs opacity-75 text-gray-400">© 2024 Barber Manager - Todos os direitos reservados.</p>
+        </div>
+    );
+}
+ 
+export default Footer;
